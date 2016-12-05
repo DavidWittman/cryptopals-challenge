cf9413a026ba3fbcc4ecfdb0a1084e9da6f46e40
David Wittman <dave@wittman.xyz>
2016-12-05 08:26:05 -0600

    [14] Complete
    Could be cleaned up a bit, but it works

--- set2/14.go
+++ set2/14.go
@@ -23,13 +23,19 @@ package set_two
 import (
 	"bytes"
 	"encoding/base64"
-	"fmt"
 	"io/ioutil"
 	"strings"
 
 	"github.com/DavidWittman/cryptopals-challenge/cryptopals"
 )
 
+var RANDOM_PREFIX []byte
+
+func init() {
+	length := cryptopals.RandomInt(1, 255)
+	RANDOM_PREFIX, _ = cryptopals.GenerateRandomBytes(length)
+
+}
 func HarderOracle(data []byte) []byte {
 	unknownString := `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXk
 gaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
@@ -42,110 +48,58 @@ dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK`
 		return []byte{}
 	}
 
-	randomPrefix, _ := cryptopals.GenerateRandomBytes(cryptopals.RandomInt(1, 255))
-
-	data = append(randomPrefix, append(data, unknownBytes...)...)
+	data = append(RANDOM_PREFIX, append(data, unknownBytes...)...)
 	result, _ := cryptopals.EncryptAESECB(data, cryptopals.RANDOM_KEY)
 	return result
 }
 
-// Determine the unknown suffix within oracle by passing it 2 repeating blocks
-// and running the oracle until we find our matching blocks in the ciphertext.
-// Then we know that our input landed on a boundary and we can determine the
-// length of the unknown input.
-func DetermineUnknownStringSize(oracle EncryptionOracle, blockSize int) int {
-	cipher := EncryptUntilMatchingBlocks(oracle, []byte{}, blockSize)
-
-	// This gives us the start of our input block. Determine the size of the unknown
-	// string by chopping off our input string and the random bytes at the front of the cipher
-	index := cryptopals.FindMatchingBlock(cipher, blockSize)
-	unknownSize := len(cipher[index+(blockSize*2):])
-	return unknownSize
-}
-
-// Generate ciphers from oracle until we find one with a matching block
-func EncryptUntilMatchingBlocks(oracle EncryptionOracle, input []byte, blockSize int) []byte {
-	// Make sure it's actually ECB
-	if !IsOracleEBC(oracle, blockSize) {
-		panic("Provided Oracle is not ECB")
-	}
-
-	// Add two identical blocks so we can locate our prefix
-	ecbTest := bytes.Repeat([]byte{0x01}, blockSize*2)
-	input = append(ecbTest, input...)
-
-	for {
+// Finds the position of our controlled input in the Oracle's output
+// This is done by creating a block of repeating characters 2 blocks
+// in length and increasing the size until we find a repeating block.
+func FindStartOfInput(oracle EncryptionOracle, blockSize int) int {
+	for i := blockSize * 2; i < blockSize*3; i++ {
+		input := bytes.Repeat([]byte{0x01}, i)
 		cipher := oracle(input)
-		// It's important to find ADJACENT matching blocks, otherwise we
-		// could potentially match blocks with values we're seeding in to
-		// the Oracle to brute force the unknown text
-		if cryptopals.FindAdjacentMatchingBlocks(cipher, blockSize) != -1 {
-			return cipher
+		if index := cryptopals.FindMatchingBlock(cipher, blockSize); index != -1 {
+			return index - (i % blockSize)
 		}
 	}
-}
 
-// Just like GenerateByteLookupTable fom ch12, but tests ECB
-func GenerateHarderLookupTable(oracle EncryptionOracle, prefix []byte, blockStart, blockEnd int) map[string]byte {
-	var i byte
-
-	result := make(map[string]byte)
-
-	blockSize := blockEnd - blockStart
-	if blockSize < 1 {
-		panic("Invalid block size")
-	}
-
-	for i = 0; i < 128; i++ {
-		input := append(prefix, i)
-
-		cipher := EncryptUntilMatchingBlocks(oracle, input, blockSize)
-		index := cryptopals.FindMatchingBlock(cipher, blockSize)
-
-		// Skip over our matching block and the short block offset
-		start := index + (blockSize * 2) + blockStart
-		end := start + blockSize
-		shortBlock := cipher[start:end]
-
-		result[string(shortBlock)] = i
-	}
-
-	return result
+	return -1
 }
 
 func BreakHarderECBOracle(oracle EncryptionOracle) ([]byte, error) {
 	var decrypted []byte
 
-	// TODO(dw): Should be detecting this
-	blockSize := 16
-	unknownLength := DetermineUnknownStringSize(oracle, blockSize)
+	blockSize := DetermineBlockSize(oracle)
+	randomPrefixLength := FindStartOfInput(oracle, blockSize)
+	// The length of our unknown bytes at the end of the Oracle output
+	unknownLength := len(oracle([]byte{})) - randomPrefixLength
+
+	// Pad the front of our input so that we're block-aligned
+	offset := blockSize - (randomPrefixLength % blockSize)
+	if offset == blockSize {
+		offset = 0
+	}
 
 	for len(decrypted) < unknownLength {
-		blockStart := len(decrypted)
+		blockStart := len(decrypted) + randomPrefixLength + offset
 		blockEnd := blockStart + blockSize
 
 		for i := blockSize - 1; i >= 0; i-- {
-			bunchOfBs := bytes.Repeat([]byte("B"), i)
+			bunchOfBs := bytes.Repeat([]byte("B"), i+offset)
 			knownPrefix := append(bunchOfBs, decrypted...)
-			lookup := GenerateHarderLookupTable(oracle, knownPrefix, blockStart, blockEnd)
+			lookup := GenerateByteLookupTable(oracle, knownPrefix, blockStart, blockEnd)
 
-			// TODO(dW): Can modify this function to just return everything after the matching blocks
-			cipher := EncryptUntilMatchingBlocks(oracle, bunchOfBs, blockSize)
-			index := cryptopals.FindMatchingBlock(cipher, blockSize)
-
-			// Skip over matching block and the short block offset
-			start := index + (blockSize * 2) + blockStart
-			end := start + blockSize
-			block := cipher[start:end]
+			cipher := oracle(bunchOfBs)
+			block := cipher[blockStart:blockEnd]
 
 			decrypted = append(decrypted, lookup[string(block)])
-
-			if lookup[string(block)] == 0 {
-				return decrypted, nil
+			if len(decrypted) == unknownLength {
+				break
 			}
-			fmt.Println(string(decrypted), len(decrypted))
 		}
 	}
 
-	return cryptopals.PKCS7Unpad(decrypted), nil
+	return decrypted, nil
 }
--- set2/14_test.go
+++ set2/14_test.go
@@ -9,12 +9,5 @@ func TestBreakHarderECBOracle(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error breaking harder oracle: %s", err)
 	}
-	t.Log(string(result))
-}
-
-func TestDetermineUnknownStringSize(t *testing.T) {
-	result := DetermineUnknownStringSize(HarderOracle, 16)
-	if result != 144 {
-		t.Errorf("Determined incorrect size. Expected: 144, Got: %d", result)
-	}
+	t.Logf("\n%s", result)
 }
