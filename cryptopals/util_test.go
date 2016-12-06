77ff98c00fae92ad09da0327344333ee2867aa16
David Wittman <dwittman@gmail.com>
2016-12-05 21:10:41 -0600

    [14] Add tests for FindMatchingBlock

--- cryptopals/util_test.go
+++ cryptopals/util_test.go
@@ -34,3 +34,29 @@ func TestGenerateRandomBytes(t *testing.T) {
 		}
 	}
 }
+
+func TestFindMatchingBlock(t *testing.T) {
+	blockSize := 16
+
+	type testCase struct {
+		input    []byte
+		expected int
+	}
+	var tests []testCase
+
+	for i := 0; i < 100; i++ {
+		prefix, _ := GenerateRandomBytes(i * blockSize)
+		input := append(prefix, bytes.Repeat([]byte{1}, blockSize*2)...)
+		tests = append(tests, testCase{input, i * blockSize})
+	}
+
+	for _, tt := range tests {
+		result := FindMatchingBlock(tt.input, 16)
+		if result != tt.expected {
+			t.Errorf("FindMatchingBytes failed. Expected: %v, Got: %v", tt.expected, result)
+		}
+		if tt.input[result] != 1 {
+			t.Errorf("First byte of matching block is not 1; may have incorrectly matched.")
+		}
+	}
+}
