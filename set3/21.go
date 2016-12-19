83d217f0dc6c6dadba997b0dc6a3a2dfd5f957f4
David Wittman <dwittman@gmail.com>
2016-12-19 08:51:07 -0600

    [21] Init

---
+++ set3/21.go
@@ -0,0 +1,12 @@
+/*
+ * Implement the MT19937 Mersenne Twister RNG
+ *
+ * You can get the psuedocode for this from Wikipedia.
+ *
+ * If you're writing in Python, Ruby, or (gah) PHP, your language is probably
+ * already giving you MT19937 as "rand()"; don't use rand(). Write the RNG
+ * yourself.
+ *
+ */
+
+package set_three
