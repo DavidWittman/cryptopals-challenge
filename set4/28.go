dc58bab547d81c30e71168951776c65719b2766d
David Wittman <dave@wittman.xyz>
2016-12-29 17:03:31 -0600

    [28] Init

---
+++ set4/28.go
@@ -0,0 +1,21 @@
+/*
+ * Implement a SHA-1 keyed MAC
+ *
+ * Find a SHA-1 implementation in the language you code in.
+
+ * Don't cheat. It won't work.
+ * Do not use the SHA-1 implementation your language already provides (for
+ * instance, don't use the "Digest" library in Ruby, or call OpenSSL; in Ruby,
+ * you'd want a pure-Ruby SHA-1).
+ *
+ * Write a function to authenticate a message under a secret key by using a
+ * secret-prefix MAC, which is simply:
+
+ *     SHA1(key || message)
+ *
+ * Verify that you cannot tamper with the message without breaking the MAC
+ * you've produced, and that you can't produce a new MAC without knowing the
+ * secret key.
+ */
+
+package set_four
