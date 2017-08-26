54185b659d34ce51fe247af47a3185409ab92a21
David Wittman <dwittman@gmail.com>
2017-08-26 00:00:02 -0500

    [35] Complete!

--- set5/35.go
+++ set5/35.go
@@ -34,15 +34,23 @@ package set_five
 
 import (
 	"log"
+	"math/big"
 	"net"
 )
 
 // Eve is an evil TCP listener which execute a MITM attack against a
 // two parties (Alice and Bob) in a DH key exchange.
 //
+// This version sets g equal to 1, which results in B and s being equal to 1:
+//
+//     B = g**b % p = 1**b % p = 1
+//     s = B**a % p = 1**a % p = 1
+//
 // `listen` is the ip:port or :port to listen on
 // `dest` is the ip:port of Bob
-func Eve35(listen, dest string) {
+func EveGEquals1(listen, dest string) {
+	// When g == 1, B == 1 && s == 1
+	g := big.NewInt(1)
 	socket, err := net.Listen("tcp", listen)
 	if err != nil {
 		panic(err)
@@ -59,15 +67,12 @@ func Eve35(listen, dest string) {
 	exchange := server.ReadMessage(DHE_MSG_EXCHANGE)
 	e := exchange.(DHExchange)
 
-	server.session = NewDHSession(e.Group.P, e.Group.G)
-	// Use p for generating Eve's session keys so that we generate the same
-	// session key between the two sessions
-	server.session.GenerateSessionKeys(e.Group.P)
+	// Manipulate g, and use g as A's public key to generate a session key of 1
+	server.session = NewDHSession(e.Group.P, g)
+	server.session.GenerateSessionKeys(g)
 
-	// Use P for the public key to make the session key predictable
-	e.PublicKey = e.Group.P
-
-	// Send exchange object with fixed key to Alice
+	// Update the exchange object to use Eve's public key
+	e.PublicKey = server.session.PublicKey
 	server.Send(e)
 
 	// Establish fixed-key MITM connection to Bob
@@ -77,15 +82,161 @@ func Eve35(listen, dest string) {
 	}
 	defer clientConn.Close()
 
-	clientSession := NewDHSession(e.Group.P, e.Group.G)
-	clientSession.PublicKey = e.Group.P
+	clientSession := NewDHSession(e.Group.P, g)
 	client := &DHClient{"EveClient", clientConn, clientSession}
 	client.Send(e)
 
-	// We don't actually need Bob's public key because our fixed-key attack has
-	// made the session key preditable.
 	_ = client.ReadMessage(DHE_MSG_EXCHANGE)
-	clientSession.GenerateSessionKeys(e.Group.P)
+	// Use g as the Public Key to generate a session key of 1
+	clientSession.GenerateSessionKeys(g)
+
+	// Intercept two messages: A -> E -> B, B -> E -> A
+	message, err := server.ReadEncrypted()
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.SendEncrypted(message)
+	if err != nil {
+		panic(err)
+	}
+
+	message, err = client.ReadEncrypted()
+	if err != nil {
+		panic(err)
+	}
+
+	err = server.SendEncrypted(message)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Eve is an evil TCP listener which execute a MITM attack against a
+// two parties (Alice and Bob) in a DH key exchange.
+//
+// This version sets g equal to p, which results in B and s being equal to 0:
+//
+//     B = p^b % p = 0
+//     s = (B ^ a) % p = (0 ^ a) % p = 0
+//
+// `listen` is the ip:port or :port to listen on
+// `dest` is the ip:port of Bob
+func EveGEqualsP(listen, dest string) {
+	// When g == p, B == 0 && s == 0
+	socket, err := net.Listen("tcp", listen)
+	if err != nil {
+		panic(err)
+	}
+	defer socket.Close()
+
+	conn, err := socket.Accept()
+	if err != nil {
+		log.Println("Error establishing connection:", err)
+		panic(err)
+	}
+
+	server := &DHClient{"Eve", conn, nil}
+	exchange := server.ReadMessage(DHE_MSG_EXCHANGE)
+	e := exchange.(DHExchange)
+
+	// Manipulate g to be p, and use 0 as A's public key to generate a session key of 0
+	server.session = NewDHSession(e.Group.P, e.Group.P)
+	server.session.GenerateSessionKeys(big.NewInt(0))
+
+	// Update the exchange object to use Eve's public key
+	e.PublicKey = server.session.PublicKey
+	server.Send(e)
+
+	// Establish fixed-key MITM connection to Bob
+	clientConn, err := net.Dial("tcp", dest)
+	if err != nil {
+		panic(err)
+	}
+	defer clientConn.Close()
+
+	clientSession := NewDHSession(e.Group.P, e.Group.P)
+	client := &DHClient{"EveClient", clientConn, clientSession}
+	client.Send(e)
+
+	_ = client.ReadMessage(DHE_MSG_EXCHANGE)
+	// Use 0 as the Public Key to generate a session key of 0
+	clientSession.GenerateSessionKeys(big.NewInt(0))
+
+	// Intercept two messages: A -> E -> B, B -> E -> A
+	message, err := server.ReadEncrypted()
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.SendEncrypted(message)
+	if err != nil {
+		panic(err)
+	}
+
+	message, err = client.ReadEncrypted()
+	if err != nil {
+		panic(err)
+	}
+
+	err = server.SendEncrypted(message)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Eve is an evil TCP listener which execute a MITM attack against a
+// two parties (Alice and Bob) in a DH key exchange.
+//
+// This version sets g equal to p-1, which results in B and s being equal to 1:
+//
+//     B = (p-1)^b % p = 1
+//     s = (B ^ a) % p = (1 ^ a) % p = 1
+//
+// `listen` is the ip:port or :port to listen on
+// `dest` is the ip:port of Bob
+func EveGEqualsPMinus1(listen, dest string) {
+	// When g == p-1, B == 1 && s == 1
+	socket, err := net.Listen("tcp", listen)
+	if err != nil {
+		panic(err)
+	}
+	defer socket.Close()
+
+	conn, err := socket.Accept()
+	if err != nil {
+		log.Println("Error establishing connection:", err)
+		panic(err)
+	}
+
+	server := &DHClient{"Eve", conn, nil}
+	exchange := server.ReadMessage(DHE_MSG_EXCHANGE)
+	e := exchange.(DHExchange)
+	// g = p - 1
+	g := new(big.Int).Sub(e.Group.P, big.NewInt(1))
+
+	// Manipulate g to be p-1, and use 1 as A's public key to generate a session key of 1
+	server.session = NewDHSession(e.Group.P, g)
+	server.session.GenerateSessionKeys(big.NewInt(1))
+
+	// Update the exchange object to use Eve's public key
+	e.PublicKey = server.session.PublicKey
+	server.Send(e)
+
+	// Establish fixed-key MITM connection to Bob
+	clientConn, err := net.Dial("tcp", dest)
+	if err != nil {
+		panic(err)
+	}
+	defer clientConn.Close()
+
+	clientSession := NewDHSession(e.Group.P, g)
+	client := &DHClient{"EveClient", clientConn, clientSession}
+	client.Send(e)
+
+	_ = client.ReadMessage(DHE_MSG_EXCHANGE)
+	// Use 0 as the Public Key to generate a session key of 0
+	clientSession.GenerateSessionKeys(big.NewInt(1))
 
 	// Intercept two messages: A -> E -> B, B -> E -> A
 	message, err := server.ReadEncrypted()
--- set5/35_test.go
+++ set5/35_test.go
@@ -6,12 +6,56 @@ import (
 )
 
 // Simulate a MITM by having Alice connect directly to Eve
-// Alice -> Eve (:3666) -> Bob (:3000)
-func TestDHMaliciousG1(t *testing.T) {
+// Alice -> Eve (:3666) -> Bob (:3333)
+func TestDHMaliciousGEquals1(t *testing.T) {
 	bobAddr := "localhost:3333"
 	eveAddr := "localhost:3666"
 
-	go Eve35(eveAddr, bobAddr)
+	go EveGEquals1(eveAddr, bobAddr)
+	go Bob(bobAddr)
+
+	// Sleep for a bit to allow time for Bob and Eve to start
+	time.Sleep(250 * time.Millisecond)
+
+	result, err := Alice(eveAddr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result != SECRET_MESSAGE {
+		t.Errorf("Alice received incorrect response.\nExpected:\t%v\nGot:\t%v", SECRET_MESSAGE, result)
+	}
+}
+
+// Simulate a MITM by having Alice connect directly to Eve
+// Alice -> Eve (:4666) -> Bob (:4444)
+func TestDHMaliciousGEqualsP(t *testing.T) {
+	bobAddr := "localhost:4444"
+	eveAddr := "localhost:4666"
+
+	go EveGEqualsP(eveAddr, bobAddr)
+	go Bob(bobAddr)
+
+	// Sleep for a bit to allow time for Bob and Eve to start
+	time.Sleep(250 * time.Millisecond)
+
+	result, err := Alice(eveAddr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result != SECRET_MESSAGE {
+		t.Errorf("Alice received incorrect response.\nExpected:\t%v\nGot:\t%v", SECRET_MESSAGE, result)
+	}
+}
+
+// Simulate a MITM by having Alice connect directly to Eve
+// Alice -> Eve (:5666) -> Bob (:5555)
+func TestDHMaliciousGEqualsPMinus1(t *testing.T) {
+	bobAddr := "localhost:5555"
+	eveAddr := "localhost:5666"
+
+	go EveGEqualsPMinus1(eveAddr, bobAddr)
 	go Bob(bobAddr)
 
 	// Sleep for a bit to allow time for Bob and Eve to start
